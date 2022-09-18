b3088730a0160c43ff3d4e7592251b07d027df01
muchen <3361977206@qq.com>
2022-09-18 16:15:22 +0800

    hot-fix commit1

--- src/main/java/GitDome3.java
+++ src/main/java/GitDome3.java
@@ -2,5 +2,6 @@ public class GitDome3 {
     public static void main(String[] args) {
         System.out.println("I play CF");
         System.out.println("I play LOL");
+        System.out.println("I play JAVA");
     }
 }
